c3406e56e339ac321631732a386b0bb7b7991d57
Brandon Gachenot <brandon1.gachenot@epitech.eu>
2021-10-24 18:40:39 +0200

    feat(loading widget): Add generic widget

---
+++ lib/widgets/loading_data.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LoadingDataScreen extends StatefulWidget {
+  const LoadingDataScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoadingDataScreenState createState() => _LoadingDataScreenState();
+}
+
+class _LoadingDataScreenState extends State<LoadingDataScreen> {
+  @override
+  Widget build(BuildContext context) {
+    const spinKit = SpinKitRotatingCircle(
+      color: Colors.deepOrange,
+      size: 200,
+    );
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Column(children: const [
+        SizedBox(
+          height: 200,
+        ),
+        spinKit,
+        SizedBox(
+          height: 150,
+        ),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text("Fetching data...."),
+        ),
+      ]),
+    );
+  }
+}
