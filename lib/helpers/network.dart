72f5304e71fdcffb9120885c08b36e8ed28173ad
Brandon Gachenot <brandon1.gachenot@epitech.eu>
2021-10-27 18:56:06 +0200

    feat(NetworkHelper): Fetch SubReddit data

--- lib/helpers/network.dart
+++ lib/helpers/network.dart
@@ -7,6 +7,7 @@ import 'package:redditech/helpers/parser.dart';
 import 'package:redditech/helpers/utils.dart';
 import 'package:http/http.dart' as http;
 import 'package:redditech/model/posts.dart';
+import 'package:redditech/model/subreddit.dart';
 import 'package:redditech/model/trophies.dart';
 import 'package:redditech/model/user.dart';
 
@@ -36,7 +37,7 @@ class NetworkHelper {
     // https://www.reddit.com/api/v1/authorize?client_id=7sv_rACJq0_AD82DfuuUag&response_type=code&state=RANDOM_STRING&redirect_uri=https://google.com&scope=identity,read,account,creddits,edit,flair,history,livemanage,modconfig,modcontributors,modflair,modlog,modmail,modothers,modposts,modself,modwiki,mysubreddits,privatemessages,report,save,structuredstyles,submit,subscribe,vote,wikiedit,wikiread&duration=permanent
     final _randomString = getRandomString(40);
     final authorizeUrl =
-    Uri.https('www.reddit.com', '/api/v1/authorize.compact', {
+        Uri.https('www.reddit.com', '/api/v1/authorize.compact', {
       'client_id': _clientID,
       'response_type': 'token',
       'state': _randomString,
@@ -49,12 +50,8 @@ class NetworkHelper {
         callbackUrlScheme: _redirectURI,
         preferEphemeral: true,
       );
-      print(_authenticateResult.toString());
-      final _redditRawResponse = Uri
-          .parse(_authenticateResult)
-          .fragment;
+      final _redditRawResponse = Uri.parse(_authenticateResult).fragment;
       final _queryResult = parseRedditAuthorizationResponse(_redditRawResponse);
-      print(_queryResult);
 
       //TODO: Handle other errors (client_id invalid, redirect_uri invalid)
       // If the user pressed the refuse button
@@ -67,9 +64,9 @@ class NetworkHelper {
       }
 
       //await _writeAccessToken(_queryResult['access_token']);
-      //await _writeAccessToken("737496854301-1e8VGWOkhWKQIdyzIKH2JtvtE6gROw"); // moi
       await _writeAccessToken(
-          "1675389028-xKniuGXth4bzYorx-HhE9TxMFyguCQ"); // Noe
+          "737496854301-ZDGqrBFo3T5ELc2TnWc1FrWiGyyq_g"); // moi
+      //await _writeAccessToken("1675389028-KVyQnBTPNPlD3OXkxGxSTg8nLbVHWw"); // Noe
       return true;
     } on PlatformException catch (_) {
       throw (Exception("Aborted by user"));
@@ -90,7 +87,7 @@ class NetworkHelper {
         headers: {
           'authorization': 'bearer ' + _token,
           'User-Agent':
-          'android:eu.epitech.redditech.redditech:v0.0.1 (by /u/M0nkeyPyth0n)',
+              'android:eu.epitech.redditech.redditech:v0.0.1 (by /u/M0nkeyPyth0n)',
         });
     if (response.statusCode == 401) {
       throw ExceptionLoginInvalid();
@@ -117,8 +114,11 @@ class NetworkHelper {
         headers: {
           'authorization': 'bearer ' + _token,
           'User-Agent':
-          'android:eu.epitech.redditech.redditech:v0.0.1 (by /u/M0nkeyPyth0n)',
+              'android:eu.epitech.redditech.redditech:v0.0.1 (by /u/M0nkeyPyth0n)',
         });
+    if (response.statusCode == 401) {
+      throw ExceptionLoginInvalid();
+    }
     Map<String, dynamic> resp = jsonDecode(response.body);
 
     User user = User(
@@ -126,7 +126,7 @@ class NetworkHelper {
         namePrefixed: resp['subreddit']['display_name_prefixed'],
         description: resp['subreddit']['public_description'],
         banner:
-        (resp['subreddit']['banner_img'] as String).isEmpty ? false : true,
+            (resp['subreddit']['banner_img'] as String).isEmpty ? false : true,
         bannerURL: resp['subreddit']['banner_img'],
         iconURL: resp['icon_img'],
         totalKarma: resp['total_karma'],
@@ -134,14 +134,42 @@ class NetworkHelper {
     return user;
   }
 
+  Future<SubReddit> fetchSubredditData(subreddit_name) async {
+    var response = await http.get(
+        Uri.parse(
+            'https://www.reddit.com/r/$subreddit_name/about.json?raw_json=1'),
+        headers: {
+          'User-Agent':
+              'android:eu.epitech.redditech.redditech:v0.0.1 (by /u/M0nkeyPyth0n)',
+        });
+    if (response.statusCode == 401) {
+      throw ExceptionLoginInvalid();
+    }
+    Map<String, dynamic> resp = jsonDecode(response.body);
+    SubReddit _subreddit = SubReddit(
+      title: resp['data']['title'],
+      subscribers: resp['data']['subscribers'],
+      icon_img: resp['data']['icon_img'],
+      display_name_prefixed: resp['data']['display_name_prefixed'],
+      community_icon_url: resp['data']['community_icon'],
+      banner_background_image_url: resp['data']['banner_background_image'],
+      description_html: resp['data']['description_html'],
+      created: resp['data']['created'],
+      banner_background_color: resp['data']['banner_background_color'],
+      mobile_banner_image: resp['data']['mobile_banner_image'],
+      public_description: resp['data']['public_description'],
+    );
+    return _subreddit;
+  }
+
   Future<List<Posts>> fetchUserBestPosts() async {
     String _token = (await _getAccessToken())!;
     var response = await http.get(
-        Uri.parse('https://oauth.reddit.com/best?raw_json=1&limit=1'),
+        Uri.parse('https://oauth.reddit.com/best?raw_json=1&limit=5'),
         headers: {
           'authorization': 'bearer ' + _token,
           'User-Agent':
-          'android:eu.epitech.redditech.redditech:v0.0.1 (by /u/M0nkeyPyth0n)',
+              'android:eu.epitech.redditech.redditech:v0.0.1 (by /u/M0nkeyPyth0n)',
         });
     if (response.statusCode == 401) {
       throw ExceptionLoginInvalid();
@@ -152,20 +180,35 @@ class NetworkHelper {
     List<Posts> _posts = [];
     List<dynamic> toto = resp['data']['children'];
     for (var element in toto) {
-      Posts _post = Posts(title: element['data']['title'],
-          created_at: element['data']['created'],
-          author: element['data']['author'],
-          author_fullname: element['data']['author_fullname'],
-          permalink: element['data']['permalink'],
-          thumbnail_url: element['data']['thumbnail'],
-          thumbnail_height: element['data']['thumbnail_height'],
-          thumbnail_width: element['data']['thumbnail_width'],
-          ups: element['data']['ups'],
-          allow_live_comments: element['data']['allow_live_comments'],
-          locked: element['data']['locked'],
-          subreddit_id: element['data']['subreddit_id'],
-          id: element['data']['id'],
-          num_comments: element['data']['num_comments']);
+      SubReddit _subreddit =
+          await fetchSubredditData(element['data']['subreddit']);
+      late String? _preview;
+      if ((element['data'] as Map<String, dynamic>).containsKey('preview')) {
+        _preview = element['data']['preview']['images'][0]['source']['url'];
+      } else {
+        _preview = null;
+      }
+      Posts _post = Posts(
+        title: element['data']['title'],
+        selftext: element['data']['selftext'],
+        created_at: element['data']['created'],
+        author: element['data']['author'],
+        author_fullname: element['data']['author_fullname'],
+        permalink: element['data']['permalink'],
+        preview: _preview,
+        thumbnail_url: element['data']['thumbnail'],
+        thumbnail_height: element['data']['thumbnail_height'],
+        thumbnail_width: element['data']['thumbnail_width'],
+        ups: element['data']['ups'],
+        allow_live_comments: element['data']['allow_live_comments'],
+        locked: element['data']['locked'],
+        subreddit: element['data']['subreddit'],
+        subreddit_name_prefixed: element['data']['subreddit_name_prefixed'],
+        subreddit_id: element['data']['subreddit_id'],
+        id: element['data']['id'],
+        num_comments: element['data']['num_comments'],
+        subReddit: _subreddit,
+      );
       _posts.add(_post);
     }
     return _posts;
