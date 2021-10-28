2e2239a88113c63eb6d17b125165cbbbf1b05d84
Brandon Gachenot <brandon1.gachenot@epitech.eu>
2021-10-29 00:58:24 +0200

    fix(LoadingData): Change spinkit

--- lib/widgets/loading_data.dart
+++ lib/widgets/loading_data.dart
@@ -11,12 +11,13 @@ class LoadingDataScreen extends StatefulWidget {
 }
 
 class _LoadingDataScreenState extends State<LoadingDataScreen> {
-  String _waitingMessage = "Fetching data....";
+  String _waitingMessage = "Fetching data...";
   late Timer _timer;
 
   void handleTimeout() {
     setState(() {
-      _waitingMessage = "Taking much time than expected...\n";
+      _waitingMessage =
+          "Fetching data is taking more time than expected...\nPlease check your network connection.\nTip: Wifi connections are more stable than cellular network.";
     });
   }
 
@@ -32,24 +33,31 @@ class _LoadingDataScreenState extends State<LoadingDataScreen> {
     super.dispose();
   }
 
+  static const _spinkit = SpinKitFadingCircle(
+    color: Colors.white,
+    size: 50.0,
+  );
+
   @override
   Widget build(BuildContext context) {
-    const spinKit = SpinKitRotatingCircle(
-      color: Colors.deepOrange,
-      size: 200,
+    const _spinKit = SpinKitSpinningLines(
+      color: Colors.black,
+      size: 160,
     );
     return Scaffold(
-      backgroundColor: Colors.grey[300],
-      body: Column(children: [
-        SizedBox(
-          height: 200,
-        ),
-        spinKit,
-        Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Text(_waitingMessage),
-        ),
-      ]),
+      body: Column(
+        children: [
+          const Expanded(
+            child: _spinKit,
+          ),
+          Expanded(
+            child: Text(
+              _waitingMessage,
+              style: const TextStyle(fontSize: 18, color: Colors.red),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
