6c2fe23d76b43a3957c562948a787d9fa3909744
Brandon Gachenot <brandon1.gachenot@epitech.eu>
2021-10-28 12:09:42 +0200

    feat(App): If device is offline, show a view

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:no_internet_check/internet_connectivity/initialize_internet_checker.dart';
+import 'package:no_internet_check/internet_connectivity/navigation_Service.dart';
 import 'package:redditech/screens/loading_view.dart';
 import "package:redditech/screens/main_view.dart";
 import 'package:redditech/screens/reddit_login_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  InternetChecker();
   runApp(MaterialApp(
+    navigatorKey: NavigationService.navigationKey,
     debugShowCheckedModeBanner: true,
     initialRoute: '/',
     routes: {
