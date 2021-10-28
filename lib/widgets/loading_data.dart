6ef61f28597770e449bda0227b43a395fc429220
Brandon Gachenot <brandon1.gachenot@epitech.eu>
2021-10-28 12:10:12 +0200

    feat(LoadingData): Customize message after time

--- lib/widgets/loading_data.dart
+++ lib/widgets/loading_data.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
@@ -9,6 +11,27 @@ class LoadingDataScreen extends StatefulWidget {
 }
 
 class _LoadingDataScreenState extends State<LoadingDataScreen> {
+  String _waitingMessage = "Fetching data....";
+  late Timer _timer;
+
+  void handleTimeout() {
+    setState(() {
+      _waitingMessage = "Taking much time than expected...\n";
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer(const Duration(seconds: 5), handleTimeout);
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     const spinKit = SpinKitRotatingCircle(
@@ -17,14 +40,14 @@ class _LoadingDataScreenState extends State<LoadingDataScreen> {
     );
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Column(children: const [
+      body: Column(children: [
         SizedBox(
           height: 200,
         ),
         spinKit,
         Padding(
           padding: EdgeInsets.all(8.0),
-          child: Text("Fetching data...."),
+          child: Text(_waitingMessage),
         ),
       ]),
     );
