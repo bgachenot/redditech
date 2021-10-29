faeffbf4699921ddf6a334c9ca930747eeef8184
Brandon Gachenot <brandon1.gachenot@epitech.eu>
2021-10-29 22:00:11 +0200

    feat(parser): Preview & Media

--- lib/helpers/parser.dart
+++ lib/helpers/parser.dart
@@ -1,5 +1,9 @@
 import 'dart:convert';
 
+import 'package:redditech/model/all_awardings.dart';
+import 'package:redditech/model/media.dart';
+import 'package:redditech/model/preview.dart';
+
 Map<String, dynamic> parseRedditAuthorizationResponse(
     String _redditRawResponse) {
   final _redditSplitRawResponse = _redditRawResponse.split('&');
@@ -12,4 +16,40 @@ Map<String, dynamic> parseRedditAuthorizationResponse(
         (i + 1 == _redditSplitRawResponse.length ? '}' : ',');
   }
   return jsonDecode(_redditRawJsonResponse);
+}
+
+List<AllAwardings>? parseAllAwardings(jsonData) {
+  return null;
+}
+
+Preview? parsePreview(Map<String, dynamic> jsonData) {
+  if (jsonData.containsKey('preview')) {
+    return Preview(
+        enabled: jsonData['preview']['enabled'],
+        source_url: jsonData['preview']['images'][0]['source']['url'],
+        source_width: jsonData['preview']['images'][0]['source']['width'],
+        source_height: jsonData['preview']['images'][0]['source']['height']);
+  } else {
+    return null;
+  }
+}
+
+Media? parseMedia(Map<String, dynamic> jsonData) {
+  if (jsonData['media'] == null) {
+    return null;
+  } else {
+    if ((jsonData['media'] as Map<String, dynamic>)
+        .containsKey('reddit_video')) {
+      return Media(
+        reddit_video: true,
+        youtube_video: false,
+        reddit_gif: false,
+        reddit_video_url: jsonData['media']['reddit_video']['fallback_url'],
+        reddit_video_height: jsonData['media']['reddit_video']['height'],
+        reddit_video_width: jsonData['media']['reddit_video']['width'],
+        reddit_video_duration: jsonData['media']['reddit_video']['duration'],
+      );
+    }
+    return null;
+  }
 }
