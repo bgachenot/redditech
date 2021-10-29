98b2f45f7c87276756fb832272da578b1b53803a
Brandon Gachenot <brandon1.gachenot@epitech.eu>
2021-10-29 22:02:01 +0200

    feat(Posts): Improve model

--- lib/model/posts.dart
+++ lib/model/posts.dart
@@ -1,94 +1,70 @@
+import 'package:redditech/model/all_awardings.dart';
+import 'package:redditech/model/media.dart';
+import 'package:redditech/model/preview.dart';
 import 'package:redditech/model/subreddit.dart';
 
 class Posts {
+  final String subreddit; // subreddit name
+  final String selftext; // Can be ''
+  final String author_fullname; // t2_ object
   final String title;
-  final String selftext;
-  final double created_at;
-  final String author;
-  final String author_fullname;
-  final String permalink;
-  final String? thumbnail_url;
-  final int? thumbnail_height;
-  final int? thumbnail_width;
-  final String? preview;
-  final int ups;
-  final bool allow_live_comments;
-  final bool locked;
-  final String subreddit;
   final String subreddit_name_prefixed;
-  final String subreddit_id;
+  final int downs;
+  final int thumbnail_height; // can be null
+  final String name; // t3_ object (is used for next)
+  final double upvote_ratio;
+  final int ups;
+  final int total_awards_received;
+  final int thumbnail_width; // can be null
+  final int score; // ups - down
+  final String thumbnail; // 'self' or 'image' or 'https://<url>'
+  final String? post_hint; //  'self' or 'link' or 'image' or 'hosted:video' (media will be fill by a reddit_video)
+  final bool is_self; // idk ?
+  final double created;
+  final String? selftext_html; // Can be null
+  final Preview? preview; // can be null
+  final List<AllAwardings>? all_awardings; // can be null
+  final String subreddit_id; // t5_ Object
   final String id;
+  final String author;
   final int num_comments;
+  final String permalink; // post permalink
+  final String url; // url to comments
+  final int subreddit_subscribers;
+  final Media? media;
+  final bool is_video; // If true, media is filled
   final SubReddit subReddit;
 
   Posts({
-    required this.title,
-    required this.selftext,
-    required this.created_at,
-    required this.author,
-    required this.author_fullname,
-    required this.permalink,
-    required this.thumbnail_url,
-    required this.thumbnail_height,
-    required this.thumbnail_width,
-    required this.preview,
-    required this.ups,
-    required this.allow_live_comments,
-    required this.locked,
     required this.subreddit,
+    required this.selftext,
+    required this.author_fullname,
+    required this.title,
     required this.subreddit_name_prefixed,
+    required this.downs,
+    required this.thumbnail_height,
+    required this.name,
+    required this.upvote_ratio,
+    required this.ups,
+    required this.total_awards_received,
+    required this.thumbnail_width,
+    required this.score,
+    required this.thumbnail,
+    required this.post_hint,
+    required this.is_self,
+    required this.created,
+    required this.selftext_html,
+    required this.preview,
+    required this.all_awardings,
     required this.subreddit_id,
     required this.id,
+    required this.author,
     required this.num_comments,
+    required this.permalink,
+    required this.url,
+    required this.subreddit_subscribers,
+    required this.media,
+    required this.is_video,
     required this.subReddit,
   });
-
-  Map<String, dynamic> toJson() {
-    return {
-      'title': title,
-      'selftext': selftext,
-      'created_at': created_at,
-      'author': author,
-      'author_fullname': author_fullname,
-      'permalink': permalink,
-      'thumbnail_url': thumbnail_url,
-      'thumbnail_height': thumbnail_height,
-      'thumbnail_width': thumbnail_width,
-      'preview': preview,
-      'ups': ups,
-      'allow_live_comments': allow_live_comments,
-      'locked': locked,
-      'subreddit': subreddit,
-      'subreddit_name_prefixed': subreddit_name_prefixed,
-      'subreddit_id': subreddit_id,
-      'id': id,
-      'num_comments': num_comments,
-    };
-  }
-
-  Posts.fromJson(Map<String, dynamic> res)
-      : title = res['title'],
-        selftext = res['selftext'],
-        created_at = res['created_at'],
-        author = res['author'],
-        author_fullname = res['author_fullname'],
-        permalink = res['permalink'],
-        thumbnail_url = res['thumbnail_url'],
-        thumbnail_height = res['thumbnail_height'],
-        thumbnail_width = res['thumbnail_width'],
-        preview = res['preview'],
-        ups = res['ups'],
-        allow_live_comments = res['allow_live_comments'],
-        locked = res['locked'],
-        subreddit = res['subreddit'],
-        subreddit_name_prefixed = res['subreddit_name_prefixed'],
-        subreddit_id = res['subreddit_id'],
-        id = res['id'],
-        num_comments = res['num_comments'],
-        subReddit = res['subReddit'];
-
-  @override
-  String toString() {
-    return 'Posts{title: $title, selftext: $selftext, created_at: $created_at, author: $author, author_fullname: $author_fullname, permalink: $permalink, thumbnail_url: $thumbnail_url, thumbnail_height: $thumbnail_height, thumbnail_width: $thumbnail_width, preview: $preview, ups: $ups, allow_live_comments: $allow_live_comments, locked: $locked, subreddit: $subreddit, subreddit_name_prefixed: $subreddit_name_prefixed, subreddit_id: $subreddit_id, id: $id, num_comments: $num_comments, subReddit, $subReddit';
-  }
 }
