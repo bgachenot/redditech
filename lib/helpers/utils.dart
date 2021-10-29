a1146ae263c046e4d7b786b725b9f913a82c4848
Brandon Gachenot <brandon1.gachenot@epitech.eu>
2021-10-29 20:39:41 +0200

    feat(Utils): prettyNumber function

--- lib/helpers/utils.dart
+++ lib/helpers/utils.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:math';
+import 'package:intl/intl.dart' as intl;
 
 class ExceptionLoginRandomString implements Exception {}
 class ExceptionLoginUserRefused implements Exception {}
@@ -10,3 +11,27 @@ Random _rng = Random();
 
 String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
     length, (_) => _chars.codeUnitAt(_rng.nextInt(_chars.length))));
+
+
+String prettyNumber(int number) {
+  String result = number.toString();
+  if (number >= 1000) {
+    number = number ~/ 1000;
+    var formatter = intl.NumberFormat('##0,0k');
+    result = formatter.format(number);
+    return result;
+  }
+  if (number >= 1000000) {
+    number = number ~/ 1000000;
+    var formatter = intl.NumberFormat('##0,0m');
+    result = formatter.format(number);
+    return result;
+  }
+  if (number >= 1000000000) {
+    number = number ~/ 1000000000;
+    var formatter = intl.NumberFormat('##0,0M');
+    result = formatter.format(number);
+    return result;
+  }
+  return result;
+}
