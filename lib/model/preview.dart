1fe6eec9638537cb772920cfde737a2116547d09
Brandon Gachenot <brandon1.gachenot@epitech.eu>
2021-10-29 20:39:52 +0200

    feat(Preview): Add Object

---
+++ lib/model/preview.dart
@@ -0,0 +1,13 @@
+class Preview {
+  final bool enabled;
+  final String source_url;
+  final int source_width;
+  final int source_height;
+
+  Preview({
+    required this.enabled,
+    required this.source_url,
+    required this.source_width,
+    required this.source_height,
+  });
+}
