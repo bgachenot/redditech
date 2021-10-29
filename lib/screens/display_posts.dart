fa8172f8515a98fbc6afa97ed8dee52a4e6cf6cd
Brandon Gachenot <brandon1.gachenot@epitech.eu>
2021-10-30 01:16:21 +0200

    fix(Video): widget was in 2/3 format

--- lib/screens/display_posts.dart
+++ lib/screens/display_posts.dart
@@ -6,6 +6,7 @@ import 'package:redditech/helpers/network.dart';
 import 'package:redditech/helpers/utils.dart';
 import 'package:redditech/model/posts.dart';
 import 'package:redditech/widgets/loading_data.dart';
+import 'package:redditech/widgets/video_widget.dart';
 import 'package:video_player/video_player.dart';
 
 class DisplayPosts extends StatefulWidget {
@@ -62,29 +63,29 @@ class _DisplayPostsState extends State<DisplayPosts> {
                 },
                 child: Column(
                   children: <Widget>[
-                      ListTile(
-                        leading: Image.network(_posts
-                                .elementAt(index)
-                                .subReddit
-                                .community_icon_url ??
-                            _posts.elementAt(index).subReddit.icon_img!),
-                        title: Text(_posts
-                            .elementAt(index)
-                            .subReddit
-                            .display_name_prefixed),
-                        subtitle: Text('u/' + _posts.elementAt(index).author),
-                        trailing: Icon(Icons.arrow_forward),
-                        onTap: () async {
-                          Navigator.pushNamed(context, '/subreddit',
-                              arguments: {
-                                'subreddit': _posts.elementAt(index).subReddit
-                              });
-                        },
-                      ),
-                    if (_posts.elementAt(index).is_video)
+                    ListTile(
+                      leading: Image.network(_posts
+                              .elementAt(index)
+                              .subReddit
+                              .community_icon_url ??
+                          _posts.elementAt(index).subReddit.icon_img!),
+                      title: Text(_posts
+                          .elementAt(index)
+                          .subReddit
+                          .display_name_prefixed),
+                      subtitle: Text('u/' + _posts.elementAt(index).author),
+                      trailing: Icon(Icons.arrow_forward),
+                      onTap: () async {
+                        Navigator.pushNamed(context, '/subreddit', arguments: {
+                          'subreddit': _posts.elementAt(index).subReddit
+                        });
+                      },
+                    ),
+                    if (_posts.elementAt(index).is_video && _posts.elementAt(index).media!.reddit_video)
                       VideoWidget(
-                          url: _posts.elementAt(index).media!.reddit_video_url!,
-                          play: true),
+                        url: _posts.elementAt(index).media!.reddit_video_url!,
+                        play: true,
+                      ),
                     if (_posts.elementAt(index).post_hint != null &&
                         _posts.elementAt(index).post_hint == 'image')
                       Column(
@@ -138,92 +139,5 @@ class _DisplayPostsState extends State<DisplayPosts> {
         },
       );
     }
-  }
-}
-
-class VideoWidget extends StatefulWidget {
-  final bool play;
-  final String url;
-
-  const VideoWidget({Key? key, required this.url, required this.play})
-      : super(key: key);
-
-  @override
-  _VideoWidgetState createState() => _VideoWidgetState();
-}
-
-class _VideoWidgetState extends State<VideoWidget> {
-  late VideoPlayerController videoPlayerController;
-  late ChewieAudioController chewieAudioPlayerController;
-  late Future<void> _initializeVideoPlayerFuture;
-
-  @override
-  void initState() {
-    super.initState();
-    videoPlayerController = VideoPlayerController.network(widget.url);
-
-    _initializeVideoPlayerFuture = videoPlayerController.initialize().then((_) {
-      //       Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
-
-      chewieAudioPlayerController = ChewieAudioController(
-          videoPlayerController: videoPlayerController,
-          autoPlay: true,
-          looping: false);
-      setState(() {});
-    });
-  } // This closing tag was missing
-
-  @override
-  void dispose() {
-    videoPlayerController.dispose();
-    chewieAudioPlayerController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initializeVideoPlayerFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return Card(
-            key: PageStorageKey(widget.url),
-            elevation: 5.0,
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Chewie(
-                    key: PageStorageKey(widget.url),
-                    controller: ChewieController(
-                      videoPlayerController: videoPlayerController,
-                      aspectRatio: 3 / 2,
-                      // Prepare the video to be played and display the first frame
-                      autoInitialize: true,
-                      looping: false,
-                      autoPlay: false,
-                      // Errors can occur for example when trying to play a video
-                      // from a non-existent URL
-                      errorBuilder: (context, errorMessage) {
-                        return Center(
-                          child: Text(
-                            errorMessage,
-                            style: const TextStyle(color: Colors.white),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
-        } else {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      },
-    );
   }
 }
