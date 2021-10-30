92c593ca941cf1fc1e734d3eaceedf1605fb973f
Brandon Gachenot <brandon1.gachenot@epitech.eu>
2021-10-31 01:20:11 +0200

    feat(SubRedditSearch): add community_icon to model

--- lib/model/subreddit_search.dart
+++ lib/model/subreddit_search.dart
@@ -1,12 +1,14 @@
 class SubRedditSearch {
   final int active_user_count;
   final String? icon_img;
+  final String? community_icon;
   final String name;
   final int subscriber_count;
 
   SubRedditSearch({
     required this.active_user_count,
     required this.icon_img,
+    required this.community_icon,
     required this.name,
     required this.subscriber_count,
   });
@@ -15,6 +17,7 @@ class SubRedditSearch {
     return {
       'active_user_count': active_user_count,
       'icon_img': icon_img,
+      'community_icon': community_icon,
       'name': name,
       'subscriber_count': subscriber_count,
     };
@@ -23,11 +26,12 @@ class SubRedditSearch {
   SubRedditSearch.fromJson(Map<String, dynamic> res)
       : active_user_count = res['active_user_count'],
         icon_img = res['icon_img'],
+        community_icon = res['community_icon'],
         name = res['name'],
         subscriber_count = res['subscriber_count'];
 
   @override
   String toString() {
-    return 'SubRedditSearch{active_user_count: $active_user_count, icon_img: $icon_img, name: $name, subscriber_count: $subscriber_count}';
+    return 'SubRedditSearch{active_user_count: $active_user_count, icon_img: $icon_img, community_icon: $community_icon, name: $name, subscriber_count: $subscriber_count}';
   }
 }
